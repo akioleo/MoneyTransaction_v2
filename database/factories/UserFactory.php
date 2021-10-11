fb7033043976f4d9e98aaf0b3a777fdea90ed019
Leonardo Akio <akioleonardo@gmail.com>
2021-10-11 12:21:10 -0300

    feature tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'document' => $this->faker->unique->numerify('#####'),
-            'account_type' =>$this->faker->randomElement([Constants::USER_ACCOUNT_TYPE_USER, Constants::USER_ACCOUNT_TYPE_SHOPKEEPER]),
+            'account_type' =>$this->faker->randomElement([Constants::USER_ACCOUNT_TYPE_USER]),
             'remember_token' => Str::random(10)
         ];
     }
--- tests/Feature/TransactionFeatureTest.php
+++ tests/Feature/TransactionFeatureTest.php
@@ -38,6 +38,20 @@ class TransactionFeatureTest extends TestCase
     {
         $payee = User::factory()->create();
 
+        $data = [
+            'payee_id' => $payee->id,
+            'value' => 100,
+            'operation_type' => Constants::TRANSACTION_OPERATION_TRANSFER
+        ];
+
+        $transactionOne = Transaction::factory()->create(['payee_id' => $this->user->id, 'payer_id'=> null, 'operation_type' => Constants::TRANSACTION_OPERATION_DEPOSIT, 'value' => 10000]);
+        $response = $this->withHeaders(['Authorization' => "Bearer " . $this->userToken])->post('api/transactions', $data)
+            ->assertJsonFragment(['payee_id' => $payee->id])->assertStatus(201);
+    }
+
+    public function test_create_transaction_transfer_unauthorized()
+    {
+        $payee = User::factory()->create();
 
         $data = [
             'payee_id' => $payee->id,
@@ -45,10 +59,41 @@ class TransactionFeatureTest extends TestCase
             'operation_type' => Constants::TRANSACTION_OPERATION_TRANSFER
         ];
 
-        $response = $this->withHeaders(['Authorization' => "Bearer " . $this->userToken])->post('api/transactions', $data)
-            ->assertStatus(422);
-        $transactionOne = Transaction::factory()->create(['payee_id' => $this->user->id, 'payer_id' => $payee->id, 'value' => 100]);
+        $response = $this->withHeaders(['Authorization' => "Bearer 1" . $this->userToken])->post('api/transactions', $data)
+            ->assertStatus(401);
+        $transactionOne = Transaction::factory()->create(['payee_id' => $this->user->id, 'payer_id'=> null, 'operation_type' => Constants::TRANSACTION_OPERATION_DEPOSIT, 'value' => 10000]);
         $response = $this->withHeaders(['Authorization' => "Bearer " . $this->userToken])->post('api/transactions', $data)
             ->assertJsonFragment(['payee_id' => $payee->id])->assertStatus(201);
+    }
+
+    public function test_create_transaction_deposit()
+    {
+        $payee = User::factory()->create();
+
+        $data = [
+            'payee_id' => $payee->id,
+            'value' => 1000,
+            'operation_type' => Constants::TRANSACTION_OPERATION_DEPOSIT
+        ];
+        //payee created from factory deposit
+        $response = $this->withHeaders(['Authorization' => "Bearer " . $this->userToken])->post('api/transactions', $data)
+            ->assertJsonFragment(['payee_id' => $payee->id])->assertStatus(201);
+        //user deposit
+        $transactionOne = Transaction::factory()->create(['payee_id' => $this->user->id, 'payer_id'=> null, 'operation_type' => Constants::TRANSACTION_OPERATION_DEPOSIT, 'value' => 10000]);
+        $response = $this->withHeaders(['Authorization' => "Bearer " . $this->userToken])->post('api/transactions', $data)
+            ->assertStatus(201);
+    }
+
+    public function test_create_transaction_withdrawl()
+    {
+        $data = [
+            'payer_id' => $this->user->id,
+            'value' => 100,
+            'operation_type' => Constants::TRANSACTION_OPERATION_WITHDRAWL
+        ];
+
+        $transactionOne = Transaction::factory()->create(['payee_id' => $this->user->id, 'payer_id'=> null, 'operation_type' => Constants::TRANSACTION_OPERATION_DEPOSIT, 'value' => 10000]);
+        $response = $this->withHeaders(['Authorization' => "Bearer " . $this->userToken])->post('api/transactions', $data)
+            ->assertStatus(201);
     }
 }
